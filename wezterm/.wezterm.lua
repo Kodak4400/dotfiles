af98119906e614a52d8359a7451d8e54a9f230b5
Kodak4400 <special0747@gmail.com>
2026-03-29 01:55:52 +0900

    wezterm追加

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,17 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.automatically_reload_config = true
+config.font_size = 12.0
+config.use_ime = true
+config.window_background_opacity = 0.75
+config.macos_window_background_blur = 20
+config.keys = {
+    {
+        mods = "CMD",
+        key = "d",
+        action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+    },
+}
+
+return config
